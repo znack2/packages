d41df97dc7179a055ed0f605d65fd62328e11c90
znack <znack@znacks-MacBook-Pro.local>
2018-01-13 07:59:42 +0300

    init package

---
+++ packages/Usedesk/EmailIntegration/src/EmailIntegrationServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Usedesk\EmailIntegration;
+
+use Illuminate\Support\ServiceProvider;
+
+class EmailIntegrationServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
